820a3252af12c44cfef6c933aa2a28feb2c658bc
Konsta Nenonen <konstanenonen@protonmail.com>
2022-11-17 23:29:43 +0200

    Added the pretty print function from the project instructions

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -36,3 +36,13 @@ function TreeFactory(array) {
     buildTree,
   };
 }
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
